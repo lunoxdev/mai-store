7960a244592c3302bbc61cde55ac7fee2a570d1d
lunoxdev <lunox.code@gmail.com>
2025-09-22 17:32:10 -0600

    Enhance AddToCartButton styling with gradients and animations for improved visual appeal; refactor button structure for better user interaction.

--- src/components/AddToCartButton.tsx
+++ src/components/AddToCartButton.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import { useCart } from '@/context/CartContext';
-import { type Product } from '@/types/product';
+import { useCart } from "@/context/CartContext";
+import { type Product } from "@/types/product";
 
 interface AddToCartButtonProps {
     product: Product;
@@ -17,9 +17,20 @@ export default function AddToCartButton({ product }: AddToCartButtonProps) {
         <button
             onClick={() => addToCart(product)}
             disabled={isDisabled}
-            className={`flex items-center justify-center rounded-md bg-blue-600 px-6 py-3 text-base font-medium text-white shadow-sm ${isDisabled ? 'opacity-50 cursor-not-allowed' : 'hover:bg-blue-700'}`}
+            className={`relative inline-flex items-center justify-center px-6 py-3 overflow-hidden font-bold text-white rounded-md shadow-2xl group ${isDisabled ? 'opacity-50 cursor-not-allowed' : 'cursor-pointer'}`}
         >
-            Add To Cart
+            <span className="absolute inset-0 w-full h-full transition duration-300 ease-out opacity-0 bg-gradient-to-br from-pink-600 via-purple-700 to-blue-400 group-hover:opacity-100"></span>
+            {/*  Top glass gradient */}
+            <span className="absolute top-0 left-0 w-full bg-gradient-to-b from-white to-transparent opacity-5 h-1/3"></span>
+            {/*  Bottom gradient */}
+            <span className="absolute bottom-0 left-0 w-full h-1/3 bg-gradient-to-t from-white to-transparent opacity-10"></span>
+            {/*  Left gradient */}
+            <span className="absolute bottom-0 left-0 w-4 h-full bg-gradient-to-r from-white to-transparent opacity-10"></span>
+            {/*  Right gradient */}
+            <span className="absolute bottom-0 right-0 w-4 h-full bg-gradient-to-l from-white to-transparent opacity-10"></span>
+            <span className="absolute inset-0 w-full h-full border border-white rounded-md opacity-10"></span>
+            <span className="absolute w-0 h-0 transition-all duration-300 ease-out bg-white rounded-full group-hover:w-56 group-hover:h-56 opacity-5"></span>
+            <span className="relative">Add To Cart</span>
         </button>
     );
 }
