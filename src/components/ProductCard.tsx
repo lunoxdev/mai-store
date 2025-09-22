74f405fbe1b01441460caa4b0a33641f19b40a93
lunoxdev <lunox.code@gmail.com>
2025-09-22 16:54:04 -0600

    Update ProductCard and Home components for improved layout; adjust product grid to display more columns and enhance ProductCard styling with a gradient background and updated text layout for better visibility.

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -10,7 +10,7 @@ export default function ProductCard({ product }: ProductCardProps) {
     return (
         <Link
             href={`/product/${product.handle}`}
-            className="group rounded-lg border border-transparent relative h-48 sm:h-60 cursor-pointer overflow-hidden"
+            className="group rounded-lg border border-transparent relative h-68 sm:h-96 cursor-pointer overflow-hidden"
         >
             {product.images.length > 0 && (
                 <Image
@@ -20,13 +20,13 @@ export default function ProductCard({ product }: ProductCardProps) {
                     className="rounded-lg absolute inset-0 transition-transform duration-300 group-hover:scale-110 object-cover"
                 />
             )}
-            <div className="absolute bottom-1.5 left-1.5 right-1.5 flex items-center gap-2 z-10 rounded-full border bg-white/50 p-0.5 text-xs sm:text-sm font-semibold text-black backdrop-blur-md">
-                <h2 className="min-w-0 line-clamp-2 grow pl-2 leading-none tracking-tight truncate">
-                    {product.name}
-                </h2>
-                <p className="flex-none rounded-full bg-blue-600 p-2 text-white shrink-0">
+            <div className="absolute bottom-0 left-0 right-0 flex flex-col z-10 bg-gradient-to-tr from-black to-90% to-transparent p-2 space-y-0.5">
+                <p className="text-sm sm:text-base">
                     ₡{parseFloat(product.price)}
                 </p>
+                <h2 className="text-sm sm:text-lg font-bold tracking-tight truncate">
+                    {product.name}
+                </h2>
             </div>
         </Link>
     );
