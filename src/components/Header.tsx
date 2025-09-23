f7c3a43f3826b8b13d74bd3acc6fd0f682b62503
lunoxdev <lunox.code@gmail.com>
2025-09-22 21:00:29 -0600

    Update Header component with enhanced styling; change title gradient colors, increase SVG icon size, and adjust cart notification position for improved aesthetics and responsiveness.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -16,7 +16,7 @@ export default function Header() {
     return (
         <header className="flex justify-between items-center p-4 mt-1 sm:mt-5">
             <Link href="/">
-                <h1 className="text-lg sm:text-3xl font-bold bg-gradient-to-r from-gray-400 via-gray-100 to-gray-400 text-transparent bg-clip-text uppercase">
+                <h1 className="text-lg sm:text-3xl font-bold bg-gradient-to-r from-pink-500 to-purple-500 text-transparent bg-clip-text uppercase">
                     Mai Store
                 </h1>
             </Link>
@@ -26,18 +26,21 @@ export default function Header() {
                     {/* SVG Shopping icon */}
                     <svg
                         xmlns="http://www.w3.org/2000/svg"
-                        width="28"
-                        height="28"
+                        width="44"
+                        height="44"
                         viewBox="0 0 256 256"
-                        className="h-8 w-8 sm:h-12 sm:w-12 cursor-pointer transition duration-200 ease-in-out hover:scale-110"
+                        className="h-8 w-8 sm:h-11 sm:w-11 cursor-pointer transition duration-200 ease-in-out hover:scale-110"
                     >
-                        <path
-                            fill="#ffffff"
-                            d="M236 69.4a16.13 16.13 0 0 0-12.08-5.4H176a48 48 0 0 0-96 0H32.08a16.13 16.13 0 0 0-12 5.4a16 16 0 0 0-3.92 12.48l14.26 120a16 16 0 0 0 16 14.12h163.25a16 16 0 0 0 16-14.12l14.26-120A16 16 0 0 0 236 69.4M96 104a8 8 0 0 1-16 0V88a8 8 0 0 1 16 0Zm32-72a32 32 0 0 1 32 32H96a32 32 0 0 1 32-32m48 72a8 8 0 0 1-16 0V88a8 8 0 0 1 16 0Z"
-                        />
+                        <g fill="#e6e6e6">
+                            <path
+                                d="m231.94 80.93l-14.25 120a8.06 8.06 0 0 1-8 7.07H46.33a8.06 8.06 0 0 1-8-7.07l-14.25-120a8 8 0 0 1 8-8.93h191.84a8 8 0 0 1 8.02 8.93"
+                                opacity=".2"
+                            />
+                            <path d="M236 69.4a16.13 16.13 0 0 0-12.08-5.4H176a48 48 0 0 0-96 0H32.08a16.13 16.13 0 0 0-12 5.4a16 16 0 0 0-3.92 12.48l14.26 120a16 16 0 0 0 16 14.12h163.25a16 16 0 0 0 16-14.12l14.26-120A16 16 0 0 0 236 69.4M128 32a32 32 0 0 1 32 32H96a32 32 0 0 1 32-32m81.76 168a.13.13 0 0 1-.09 0H46.25L32.08 80H80v24a8 8 0 0 0 16 0V80h64v24a8 8 0 0 0 16 0V80h48Z" />
+                        </g>
                     </svg>
                     {cartCount > 0 && (
-                        <span className="absolute -top-0 -right-1.5 bg-blue-600 text-xs sm:text-sm font-bold rounded-full h-5 w-5 flex items-center justify-center">
+                        <span className="absolute top-1 -right-1 bg-gradient-to-br from-pink-500 to-purple-500 text-xs sm:text-sm font-extrabold rounded-full h45 w-4 flex items-center justify-center">
                             {cartCount}
                         </span>
                     )}
