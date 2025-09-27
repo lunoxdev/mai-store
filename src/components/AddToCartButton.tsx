cdbc92eeb3c1da5669d939567e3e74c7c5aef443
lunoxdev <lunox.code@gmail.com>
2025-09-27 11:42:43 -0600

    add missing translations

--- src/components/AddToCartButton.tsx
+++ src/components/AddToCartButton.tsx
@@ -19,7 +19,7 @@ export default function AddToCartButton({ product }: AddToCartButtonProps) {
             disabled={isDisabled}
             className={`border text-sm sm:text-xl px-5 sm:px-6 py-3 sm:py-4 rounded-md hover:bg-black hover:text-white transition duration-300 ease-in-out ${isDisabled ? 'opacity-50 cursor-not-allowed' : 'cursor-pointer'}`}
         >
-            Add To Cart
+            Añadir al carrito
         </button>
     );
 }
