a8a8d17f4db59da85482194b3d76552776e8372b
lunoxdev <lunox.code@gmail.com>
2025-09-27 13:23:11 -0600

    Remove InfiniteMovingText component from RootLayout to streamline the layout structure.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,6 @@ import Header from "@/components/Header";
 import { CartProvider } from '@/context/CartContext';
 import { Analytics } from '@vercel/analytics/react';
 import Background from "@/components/Background";
-import { InfiniteMovingText } from "@/components/ui/InfiniteMovingText";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -36,24 +35,6 @@ export default function RootLayout({
         <div className="w-full sm:w-5xl">
           <CartProvider>
             <Header />
-            <div className="flex flex-col antialiased items-center justify-center relative overflow-hidden">
-              <InfiniteMovingText
-                items={[
-                  { id: "1", name: "Pants", handle: "pants" },
-                  { id: "2", name: "Shoes", handle: "shoes" },
-                  { id: "3", name: "T-Shirts", handle: "t-shirts" },
-                  { id: "4", name: "Electronics", handle: "electronics" },
-                  { id: "5", name: "Toys", handle: "toys" },
-                  { id: "6", name: "Books", handle: "books" },
-                  { id: "7", name: "Home Decor", handle: "home-decor" },
-                  { id: "8", name: "Sports", handle: "sports" },
-                ]}
-                direction="left"
-                speed="normal"
-                pauseOnHover={false}
-                className="py-2"
-              />
-            </div>
             <main>
               {children}
             </main>
